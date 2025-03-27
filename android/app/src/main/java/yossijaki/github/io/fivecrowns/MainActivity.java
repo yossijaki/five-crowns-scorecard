30695f13b3901861eb67e565d13db2cc819715b1
Yossijaki Jorge Astilleros Chavez <yossijaki@gmail.com>
2025-03-27 16:35:07 -0600

    Capacitor added and Android project created

---
+++ android/app/src/main/java/yossijaki/github/io/fivecrowns/MainActivity.java
@@ -0,0 +1,5 @@
+package yossijaki.github.io.fivecrowns;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'yossijaki.github.io.fivecrowns',
+  appName: 'Five Crowns Companion: Party Score',
+  webDir: 'dist'
+};
+
+export default config;
