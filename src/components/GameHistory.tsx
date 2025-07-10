099e34e4109fd1ff1bee7f794c4d436bbdd196d8
Yossijaki Jorge Astilleros Chavez <yossijaki@gmail.com>
2025-07-10 16:44:47 -0600

    feat(Dialogs): background shadows less traslucent

--- src/components/GameHistory.tsx
+++ src/components/GameHistory.tsx
@@ -90,7 +90,7 @@ export function GameHistoryList({
                 onClose={() => setIsDeleteGameDialogOpen(false)}
                 className="relative z-50"
               >
-                <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+                <div className="fixed inset-0 bg-black/50" aria-hidden="true" />
                 <div className="fixed inset-0 flex items-center justify-center p-4">
                   <DialogPanel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-dark-100 p-6 text-left align-middle shadow-xl transition-all">
                     <DialogTitle className="font-bold">
--- src/components/Scorecard.tsx
+++ src/components/Scorecard.tsx
@@ -173,7 +173,7 @@ export function Scorecard({
               onClose={() => setIsFinishGameDialogOpen(false)}
               className="relative z-50"
             >
-              <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+              <div className="fixed inset-0 bg-black/50" aria-hidden="true" />
               <div className="fixed inset-0 flex items-center justify-center p-4">
                 <DialogPanel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-dark-100 p-6 text-left align-middle shadow-xl transition-all">
                   <DialogTitle className="font-bold">
@@ -322,7 +322,7 @@ export function Scorecard({
         onClose={() => setPlayerToDelete(null)}
         className="relative z-50"
       >
-        <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+        <div className="fixed inset-0 bg-black/50" aria-hidden="true" />
         <div className="fixed inset-0 flex items-center justify-center p-4">
           <DialogPanel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-dark-100 p-6 text-left align-middle shadow-xl transition-all">
             <DialogTitle className="text-lg font-medium leading-6 text-gray-200">
